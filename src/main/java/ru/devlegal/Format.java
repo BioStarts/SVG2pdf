c4fe96cf26bb0650e7d54728033ee8e4bff78aa8
vstartsev <vstartsev@amulex.ru>
2019-04-03 19:07:26 +0300

    first commit

---
+++ src/main/java/ru/devlegal/Format.java
@@ -0,0 +1,4 @@
+package ru.devlegal;
+
+public class Format {
+}
---
+++ src/main/java/ru/devlegal/SVGExportException.java
@@ -0,0 +1,8 @@
+package ru.devlegal;
+
+import org.apache.batik.transcoder.TranscoderException;
+
+public class SVGExportException extends Throwable {
+    public SVGExportException(TranscoderException ex) {
+    }
+}
